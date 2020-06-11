871e700da7affb0ccada8ce0f1d5313e009559d2
JacksonFB123 <64597826+JacksonFB123@users.noreply.github.com>
2020-06-11 12:33:30 -0400

    Update paint.py

--- paint.py
+++ paint.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from tkinter import messagebox as mbox
-import turtle
+import turtle, time
 version = '1.0'
 canvas = turtle.Pen()
 tk = Tk()
+def _info(): 
+    newWindow = Toplevel(tk) 
+    newWindow.title("PyPaint Info")  
+    newWindow.geometry("400x100") 
+    Label(newWindow, text ='''- PyPaint -
+- Version '''+version+''' -
+- Made by Jackson Baker in 2020 -
+A GUI Baced Painting Application written in Python using Tkinter.
+For more info go to https://github.com/ and search "PyPaint".
+(Just so you know, this program is in it's beta testing ''').pack()
 def _forward():
     canvas.forward(10)
 def _back():
@@ -25,6 +35,7 @@ btn4 = Button(tk, text='Right', command=_right)
 btn5 = Button(tk, text='Pen On', command=_down)
 btn6 = Button(tk, text='Pen Off', command=_up)
 btn7 = Button(tk, text='Reset Canvas', command=_clear)
+btn8 = Button(tk, text='Program Info', command=_info)
 def packall():
     btn1.pack()
     btn2.pack()
@@ -33,5 +44,6 @@ def packall():
     btn5.pack()
     btn6.pack()
     btn7.pack()
+    btn8.pack()
 packall()
 mbox.showinfo('PyPaint v'+version, 'Welcome to PyPaint v'+version+'!')
